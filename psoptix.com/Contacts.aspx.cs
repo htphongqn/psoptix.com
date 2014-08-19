a5c34e3956c3b3bab3ef00866591512ff5b2f698
Bùi Văn Chí <bvchi2907@gmail.com>
2014-08-20 00:27:15 +0700

    Sent mail complete

--- psoptix.com/Contacts.aspx.cs
+++ psoptix.com/Contacts.aspx.cs
@@ -31,14 +31,27 @@ namespace psoptix.com
             int i = _db.ExecuteInt(sql);
             try
             {
-                System.Net.Mail.SmtpClient smtp = new SmtpClient()
-                {
-                    Host = "smtpout.secureserver.net",
-                    Port = 465,
-                    EnableSsl = ("TLS" != ""),
-                    UseDefaultCredentials = true
-                };
-                smtp.Send(new MailMessage(txtEmail.Text, "Info@perfectsqs.com", txtName.Text + " - " + txtCity.Text, txtMessage.Text));
+                //System.Net.Mail.SmtpClient smtp = new SmtpClient()
+                //{
+                //    Host = "smtpout.secureserver.net",
+                //    Port = 465,
+                //    EnableSsl = ("TLS" != ""),
+                //    UseDefaultCredentials = true
+                //};
+                //smtp.Send(new MailMessage(txtEmail.Text, "Info@perfectsqs.com", txtName.Text + " - " + txtCity.Text, txtMessage.Text));
+
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(txtEmail.Text);
+                mail.To.Add("info@perfectsqs.com");
+                mail.Subject = "Contact Us";
+                mail.IsBodyHtml = true;
+                mail.Body = "Name: " + txtName.Text + "<br />";
+                mail.Body += "City: " + txtCity.Text + "<br />";
+                mail.Body += "Email: " + txtEmail.Text + "<br />";
+                mail.Body += "Comments: " + txtMessage.Text + "<br />";
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtpout.secureserver.net";
+                smtp.Send(mail);
             }
             catch { }
             //if (i != -1)
